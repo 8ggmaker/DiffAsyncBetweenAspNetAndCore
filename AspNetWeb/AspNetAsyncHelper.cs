936ce42567a1afc9d1cab2489292f2c568c909a7
zsybupt <zsybupt@outlook.com>
2020-01-07 16:37:15 +0800

    refactor code and add comments

--- AspNetWeb/AspNetAsyncHelper.cs
+++ AspNetWeb/AspNetAsyncHelper.cs
@@ -19,23 +19,47 @@ namespace AspNetWeb
         public async Task DoAsyncJobWithDiagnosticSyncContext()
         {
             HttpApplication syncContext = HttpContext.Current.ApplicationInstance;
-            SynchronizationContext.SetSynchronizationContext(new EmptySynchronizationContext());
-            await DoIOJob();
-            try
-            {
-                var type = typeof(HttpApplication);
-                var method = type.GetTypeInfo().GetDeclaredMethod("OnThreadEnterPrivate");
-                if (method != null)
-                {
-                    method.Invoke(syncContext, new object[] { true });
-                }
-            }
-            catch(Exception ex) //will throw exception here
-            {
-                throw ex;
-            }
-          
+            SynchronizationContext.SetSynchronizationContext(new AspNetSynchronizationContextSimulator(syncContext));
+            await DoIOJob();                   
             Interlocked.Increment(ref count);
         }
+
+        internal class AspNetSynchronizationContextSimulator : SynchronizationContext
+        {
+            /// <summary>
+            /// HttpApplication implements ISyncContext, which is internal
+            /// https://referencesource.microsoft.com/#System.Web/Util/ISyncContext.cs
+            /// </summary>
+            private HttpApplication syncContext;
+            public AspNetSynchronizationContextSimulator(HttpApplication syncContext)
+            {
+                this.syncContext = syncContext;
+            }
+
+            public override void Post(SendOrPostCallback d, object state)
+            {
+                // simulate what AspNetSynchronizationContext does when call await callback
+                // try to enter current synccontext to ensure only one callback get executed at each time,
+                // https://referencesource.microsoft.com/#System.Web/Util/SynchronizationHelper.cs,157
+                // but when response has returned, ISyncContex.Enter will raise an exception and cause callback can not get invoked properly
+                try
+                {
+                    var type = typeof(HttpApplication);
+                    var method = type.GetTypeInfo().GetDeclaredMethod("OnThreadEnterPrivate");
+                    if (method != null)
+                    {
+                        method.Invoke(syncContext, new object[] { true }); // throw exception here
+                    }
+
+                    d(state); // can not get executed
+                }
+                catch(Exception ex)
+                {
+                    throw ex;
+                }
+            }
+        }
     }
+
+
 }
